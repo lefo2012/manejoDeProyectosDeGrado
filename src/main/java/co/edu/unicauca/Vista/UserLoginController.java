8a1ec605254c8e2c6318ff59d9ce022f606abcec
LEFO <luisfierro@unicauca.edu.co>
2025-08-30 13:13:09 -0500

    Falta incorporar la vista con el back para que funcione login y register ya esta aplicado el patron MVC

---
+++ src/main/java/co/edu/unicauca/Vista/UserLoginController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package co.edu.unicauca.Vista;
+
+import co.edu.unicauca.main.Main;
+import java.io.IOException;
+
+/**
+ * FXML Controller class
+ *
+ * @author LEFO
+ */
+public class UserLoginController  {
+
+
+    public void irARegistrarse() throws IOException {
+        
+        Main.setRoot("UserRegister");
+        
+    }    
+    
+}
---
+++ src/main/java/co/edu/unicauca/Vista/UserRegisterController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.unicauca.Vista;
+
+import co.edu.unicauca.main.Main;
+import java.io.IOException;
+
+/**
+ *
+ * @author PixelBot Gaming
+ */
+public class UserRegisterController {
+    public void irALogin() throws IOException
+    {
+    
+        Main.setRoot("UserLogin");
+    
+    }
+}
